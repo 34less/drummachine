d9090eb6b1b7642f76cbdfe85411634f91003d05
Alessandro <alessandro.balzarini.9@gmail.com>
2018-06-20 06:40:43 +0200

    modified server_ip_address, removed process-env

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const morgan = require('morgan');
 
 const presetHandler = require('./presetHandler');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address =  '0.0.0.0';
  
 
 
